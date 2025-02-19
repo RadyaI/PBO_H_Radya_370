aa44f9c5a7df77b907fc0536daa8187e9b737d68
RadyaI <radyaiftikhar@gmail.com>
2025-02-19 20:19:08 +0700

    First Commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       Scanner ambilScan = new Scanner(System.in);
+
+       System.out.print("Masukkan nama: ");
+       String nama = ambilScan.nextLine();
+
+        System.out.println("Nama: " + nama);
+    }
+}
