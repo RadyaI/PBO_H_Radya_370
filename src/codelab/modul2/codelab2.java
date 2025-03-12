f7d837cda34a8035a039c839271d17e18ac88d8c
RadyaI <radyaiftikhar@gmail.com>
2025-03-12 20:25:01 +0700

    Kelar

--- src/codelab/modul2/codelab2.java
+++ src/codelab/modul2/codelab2.java
@@ -1,62 +1,57 @@
 package codelab.modul2;
 
-import java.text.DecimalFormat;
-
 class RekeningBank {
-    String nomorRekening;
-    String namaPemilik;
-    double saldo;
+    String nomorRekening, namaPemilik;
+    long saldo;
 
-    DecimalFormat formatter = new DecimalFormat("#,##0.0");
-
-    void tampilkanInfo() {
-        System.out.println("Nomor Rekening: " + nomorRekening);
-        System.out.println("Nama Pemilik: " + namaPemilik);
-        System.out.println("Saldo: Rp" + formatter.format(saldo));
-        System.out.println();
+    String tampilkanInfo() {
+        return "Nomor Rekening: " + nomorRekening + "\n" + "Nama Pemilik: " + namaPemilik + "\n" + "Saldo: Rp." + saldo
+                + "\n";
     }
 
-    void masukanInfo(String nomor, String nama, double saldoAwal) {
-        nomorRekening = nomor;
-        namaPemilik = nama;
-        saldo = saldoAwal;
-        tampilkanInfo();
+    void setorUang(long jumlahSetor) {
+        saldo += jumlahSetor;
+        System.out.printf("%s menyetor Rp.%d. Saldo sekarang %d\n", namaPemilik, jumlahSetor, saldo);
     }
 
-    void setorUang(double jumlah) {
-        saldo += jumlah;
-        System.out.println(namaPemilik + " menyetor Rp" + formatter.format(jumlah) + ". Saldo sekarang: Rp"
-                + formatter.format(saldo));
-    }
-
-    void tarikUang(double jumlah) {
-        if (jumlah > saldo) {
-            System.out.println(namaPemilik + " menarik Rp" + formatter.format(jumlah)
-                    + ". (Gagal, saldo tidak mencukupi) Saldo saat ini: Rp" + formatter.format(saldo));
+    void tarikUang(long jumlahTarik) {
+        if (saldo < jumlahTarik) {
+            System.out.printf("%s menarik %d. (GAGAL, Saldo tidak mencukupi) saldo sekarang %d\n", namaPemilik,
+                    jumlahTarik, saldo);
         } else {
-            saldo -= jumlah;
-            System.out.println(namaPemilik + " menarik Rp" + formatter.format(jumlah) + "(Berhasil) Saldo sekarang: Rp"
-                    + formatter.format(saldo));
+            saldo -= jumlahTarik;
+            System.out.printf("%s menarik %d. (BERHASIL) saldo sekarang %d\n", namaPemilik, jumlahTarik, saldo);
         }
     }
 }
 
 public class codelab2 {
     public static void main(String[] args) {
-        RekeningBank rekening1 = new RekeningBank();
-        RekeningBank rekening2 = new RekeningBank();
+        RekeningBank rek1 = new RekeningBank();
+        RekeningBank rek2 = new RekeningBank();
 
-        rekening1.masukanInfo("202410370110381", "Aini", 25000000);
-        rekening2.masukanInfo("202410370110370", "Radya", 50000000);
+        rek1.namaPemilik = "Muhammad Radya Iftikhar";
+        rek1.nomorRekening = "202410370110370";
+        rek1.saldo = 50_000_000l;
 
-        rekening1.setorUang(5000000);
-        rekening2.setorUang(10000000);
+        rek2.namaPemilik = "Radya Second Account";
+        rek2.nomorRekening = "2024103701103702";
+        rek2.saldo = 40_000_000l;
 
-        rekening1.tarikUang(35000000);
-        rekening2.tarikUang(20000000);
+        System.out.println(rek1.tampilkanInfo());
+        System.out.println(rek2.tampilkanInfo());
+
+        rek1.setorUang(50_000_000l);
+        rek2.setorUang(10_000_000l);
 
         System.out.println();
-        rekening1.tampilkanInfo();
-        rekening2.tampilkanInfo();
+
+        rek1.tarikUang(20_000_000l);
+        rek2.tarikUang(55_000_000l);
+
+        System.out.println();
+
+        System.out.println(rek1.tampilkanInfo());
+        System.out.println(rek2.tampilkanInfo());
     }
 }
